5c7a5194390b74eeb9838d80b52cc4d7919c0079
Mayank Bansal <sunny.bansal@gmail.com>
2018-04-09 20:29:05 -0500

    AS4 finished

--- assignment-4/README.md
+++ assignment-4/README.md
@@ -3,6 +3,49 @@ Mayank Bansal<br>
 A20392482<br>
 mbansal5@hawk.iit.edu
 
+## Executing
+Run
 ```
-IN PROGRESS
+python3 pagerank.py > output.txt
+```
+
+Uncomment out the testcase you want to run.
+
+```test1.txt``` & ```text2.txt``` are given test cases.
+
+```test3.txt``` is from the sample in the slides.
+
+```test4.txt``` is a random testcase
+
+### Saving to File
+Redirecting using,
+
+```
+python3 pagerank.py > outputs/output.txt
+```
+
+For the random testing,
+
+```
+python3 pagerank.py > outputs/output_random.txt
+```
+
+will save the output to output.txt
+
+## Random Testing
+To run random tests, uncomment the following piece of code
+
+```
+###################################################################################
+#                                                                                 #
+#  Uncomment this for Random Testing                                              #
+#                                                                                 #
+#  test_random(int max_n) where max_n is the maximum range for random page count  #
+#  for quick tests, keep max_n < 100                                              #
+#  this function has been tested for max_n = 1000 (2-3 minutes running time)      #
+#                                                                                 #
+###################################################################################
+
+i.test_random()
+
 ```
--- assignment-4/pagerank.py
+++ assignment-4/pagerank.py
@@ -2,6 +2,7 @@
 import re
 import collections
 import operator
+from random import *
 
 
 class PageRank:
@@ -12,6 +13,9 @@ class PageRank:
 	# function to implement PageRank algorithm
 	# input_file - input file that follows the format provided in the assignment description
 	def page_rank(self, input_file):
+
+		print("\nOutput for file", input_file)
+
 		lines = open(input_file)
 
 		page_count = int(lines.readline())
@@ -42,7 +46,7 @@ class PageRank:
 				if link_counts[i] != 0:
 					transition_matrix[i][j] = adjacency_matrix[i][j] / link_counts[i]
 				else:
-					transition_matrix[i][j] = 1/page_count
+					transition_matrix[i][j] = 1 / page_count
 
 				transition_matrix[i][j] *= (1 - self.alpha)
 				transition_matrix[i][j] += self.alpha / page_count
@@ -88,9 +92,82 @@ class PageRank:
 		for k, v in enumerate(sorted_ranking):
 			print("#", k + 1, v)
 
+	# function to run random tests
+	def test_random(self, max_n):
+
+		page_count = randint(1, max_n)
+		edge_count = randint(0, page_count * page_count)
+
+		edge_list = []
+
+		for i in range(0, edge_count):
+			src = randint(0, page_count - 1)
+			dst = randint(0, page_count - 1)
+
+			edge = [src, dst]
+
+			if edge not in edge_list:
+				edge_list.append(edge)
+
+		edge_count = len(edge_list)
+
+		test_file = open("tests/test_random.txt", "w")
+
+		test_file.write("%d\n" % page_count)
+		test_file.write("%d\n" % edge_count)
+		for edge in edge_list:
+			test_file.write("%d %d\n" % (edge[0], edge[1]))
+
+		test_file.close()
+
+		print("Page Count: ", page_count)
+		print("Edge Count: ", edge_count)
+
+		self.page_rank("tests/test_random.txt")
+
 
 i = PageRank()
-i.page_rank("test1.txt")
-i.page_rank("test2.txt")
-i.page_rank("test3.txt")
-i.page_rank("test4.txt")
+
+###################
+#                 #
+#  Sample Test 1  #
+#                 #
+###################
+
+# i.page_rank("tests/test1.txt")
+
+###################
+#                 #
+#  Sample Test 1  #
+#                 #
+###################
+
+# i.page_rank("tests/test2.txt")
+
+#################################
+#                               #
+#  Sample Test 3 - From Slides  #
+#                               #
+#################################
+
+# i.page_rank("tests/test3.txt")
+
+###############################
+#                             #
+#  Sample Test 3 - Arbitrary  #
+#                             #
+###############################
+
+# i.page_rank("tests/test4.txt")
+
+###################################################################################
+#                                                                                 #
+#  Uncomment this for Random Testing                                              #
+#                                                                                 #
+#  test_random(int max_n) where max_n is the maximum range for random page count  #
+#  for quick tests, keep max_n < 100                                              #
+#  this function has been tested for max_n = 1000 (2-3 minutes running time)      #
+#                                                                                 #
+###################################################################################
+
+i.test_random(300)
